61ff2ee1667d84b6befadb74575ef69859fcf59d
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2019-05-28 12:33:59 -0300

    Validador de dni criado, metodo cadastraPessoa em andamento.

--- src/controladores/ControladorDePessoasEDeputados.java
+++ src/controladores/ControladorDePessoasEDeputados.java
@@ -1,5 +1,28 @@
 package controladores;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import entidades.Pessoa;
+import metodosAuxiliares.ValidaDni;
+
 public class ControladorDePessoasEDeputados {
 
+	private Map<String, Pessoa> pessoas;
+
+	public ControladorDePessoasEDeputados() {
+		this.pessoas = new HashMap<>();
+	}
+
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses) {
+		if (nome == null || nome.equals("")) {
+			throw new IllegalArgumentException("Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
+		} else if (dni == null || dni.equals("")) {
+			throw new IllegalArgumentException("Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
+		} else if (!ValidaDni.validaDni(dni)) {
+			throw new IllegalArgumentException("Erro ao cadastrar pessoa: dni invalido");
+		} else if (estado == null || estado.equals("")) {
+			throw new IllegalArgumentException("Erro ao cadastrar pessoa: estado nao pode ser vazio ou nulo");
+		}
+	}
 }
---
+++ src/metodosAuxiliares/ValidaDni.java
@@ -0,0 +1,27 @@
+package metodosAuxiliares;
+
+public class ValidaDni {
+	
+	public static boolean validaDni(String dni) {
+		if(dni.trim().length() != 11) {
+			return false;
+		}
+
+		String[] dniFormatado = dni.split("-");
+		
+		if(dniFormatado.length != 2) {
+			return false;
+		}else if(dniFormatado[0].length() != 9 || dniFormatado[1].length() != 1) {
+			return false;
+		}
+		
+		try {
+			Integer.parseInt(dniFormatado[0]);
+			Integer.parseInt(dniFormatado[1]);
+		} catch(NumberFormatException e) {
+			return false;
+		}
+		
+		return true;
+	}
+}
