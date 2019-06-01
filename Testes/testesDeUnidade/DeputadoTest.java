df1b2898b90ec9082be0593fb66c5cda509181a9
JacksonMateus <jackson.aguiar@ccc.edu.ufcg.br>
2019-06-01 01:09:48 -0300

    Inicializado testes de unidade

--- Testes/testesDeUnidade/DeputadoTest.java
+++ Testes/testesDeUnidade/DeputadoTest.java
@@ -2,13 +2,35 @@ package testesDeUnidade;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import entidades.Deputado;
+
 class DeputadoTest {
+	Deputado deputado;
+	
+	@BeforeEach
+	void iniciaDeputado() {
+		deputado = new Deputado("30/11/2010");
+		
+	}
+	@Test
+	void testDeputado() {
+		Deputado deputado2 = new Deputado("30/11/2010");
+		assertEquals(deputado.getDataDeInicio(), deputado2.getDataDeInicio());
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSetLeisAprovadas() {
+		deputado.setLeisAprovadas(2);
+		assertEquals(2, deputado.getLeisAprovadas());
+	}
+
+	@Test
+	void testToString() {
+		assertEquals("POL:", deputado.toString());
+
 	}
 
 }
--- Testes/testesDeUnidade/ValidaDniTest.java
+++ Testes/testesDeUnidade/ValidaDniTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import metodosAuxiliares.ValidaDni;
+
 class ValidaDniTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void validaDni() {
+		assertFalse(ValidaDni.validaDni("1111111111-A"));		
+		assertFalse(ValidaDni.validaDni("A111111111-0"));
+		assertTrue(ValidaDni.validaDni("111111111-0"));
+		
 	}
 
 }
