19c41933085143e3de064bd573f7e5207828b92c
lucas-lucena <lucas.lucena@ccc.ufcg.edu.br>
2019-06-04 09:53:56 -0300

    Alterando o validaDataDeInicio, fase01

--- src/metodosAuxiliares/ValidaDataDeInicio.java
+++ src/metodosAuxiliares/ValidaDataDeInicio.java
@@ -5,20 +5,37 @@ import java.time.format.DateTimeFormatter;
 
 public class ValidaDataDeInicio {
 	
-	public static boolean validaDataDeInicio(String dataDeInicio) {
+	//Não ta funcionando.
+	public boolean validaDataDeInicio(String dataDeInicio) {
 		if(dataDeInicio.trim().length() != 8) {
 			return false;
 		}
-
+		
+		String dia = dataDeInicio.substring(0, 2);
+		String mes = dataDeInicio.substring(2, 4);
+		String ano = dataDeInicio.substring(4, 8);
+		
+		String data = dia + "/" + mes + "/" + ano;
+		System.out.println("flag " +data);
 		try {
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/mm/yyyy");
-			LocalDate formatedData = LocalDate.parse(dataDeInicio, formatter);
-			dataDeInicio = formatter.format(formatedData);
+			
+			LocalDate localDate = LocalDate.now();
+			
+			int resultDia = Integer.parseInt(dia);
+			System.out.println("flag 1");
+			int resultMes = Integer.parseInt(mes);
+			System.out.println("flag 2");
+			int resultAno = Integer.parseInt(ano);
+			System.out.println("flag 3");
+			String resultData = resultDia + "/" + resultMes + "/" + resultAno;
+			
+			
+			
+			return true;
 		}
 		catch(Exception e) {
 			return false;
 		}
 
-		return true;
 	}
 }
