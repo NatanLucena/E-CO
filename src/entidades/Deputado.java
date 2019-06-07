0d0ab9040b7be21b46f906b37be2afc781039c5e
JacksonMateus <jackson.aguiar@ccc.ufcg.edu.br>
2019-06-07 14:54:55 -0300

    documentação iniciada

--- src/entidades/Deputado.java
+++ src/entidades/Deputado.java
@@ -3,27 +3,48 @@ package entidades;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+/**
+ * Responsavel por representar um Deputado
+ * @author NatanLucena, CayoViegas, lucas-lucena, JacksonMateus
+ *
+ */
 public class Deputado implements Funcao {
 
-
+	/**
+	 * Data de inicio que a pessoa exerceu a funcao de deputado
+	 */
 	private String dataDeInicio;
+	
+	/**
+	 * Quantidade de leis que o deputado aprovou
+	 */
 	private int leisAprovadas;
 
-
+	/**
+	 * Inicia o deputado a partir da data de inicio da pessoa na funcao de deputado
+	 * @param dataDeInicio que a pessoa exerceu a funcao de deputado
+	 */
 	public Deputado(String dataDeInicio) {
 		this.dataDeInicio = dataDeInicio;
 		this.leisAprovadas = 0;
 	}
-
+	/**
+	 * Retorna as quantidade de leis aprovadas pelo deputado
+	 */
 	public int getLeisAprovadas() {
 		return leisAprovadas;
 	}
-
+	/**
+	 * Altera quantidade de leis aprovadas pelo deputado
+	 * @param leisAprovadas nova quantidade de leis aprovadas pelo deputado
+	 */
 	public void setLeisAprovadas(int leisAprovadas) {
 		this.leisAprovadas = leisAprovadas;
 	}
-
+	
+	/**
+	 * Retorna a data de inicio da pessoa como Deputado
+	 */
 	public String getDataDeInicio() {
 		String dia = dataDeInicio.substring(0, 2);
 		String mes = dataDeInicio.substring(2, 4);
@@ -32,8 +53,4 @@ public class Deputado implements Funcao {
 		return dia + "/" + mes + "/" + ano;
 	}
 
-	@Override
-	public String toString() {
-		return "POL: ";
-	}
 }
--- src/entidades/Pessoa.java
+++ src/entidades/Pessoa.java
@@ -1,16 +1,43 @@
 package entidades;
 
 import metodosAuxiliares.ValidaDni;
-
+/**
+ * É responsavél por representar um cliente
+ * @author NatanLucena, CayoViegas, lucas-lucena, JacksonMateus
+ *
+ */
 public class Pessoa {
-
+	/**
+	 * Armazena nome da pessoa 
+	 */
 	private String nome;
+	/**
+	 * Armazena dni da pessoa
+	 */
 	private String dni;
+	/**
+	 * Armazena estado da pessoa
+	 */
 	private String estado;
+	/**
+	 * Armazena interesses da pessoa
+	 */
 	private String interesses;
+	/**
+	 * Armazena partido da pessoa
+	 */
 	private String partido;
+	/**
+	 * Armazena as funçoes que a pessoa exerce,podendo ser pessoa uma pessoa simples ou deputado
+	 */
 	private Funcao funcao;
-
+	/**
+	 * Inicia uma pessoa a partir do nome, dni, estado e interesses.
+	 * @param nome da pessoa
+	 * @param dni da pessoa
+	 * @param estado da pessoa 
+	 * @param interesses da pessoa
+	 */
 	public Pessoa(String nome, String dni, String estado, String interesses) {
 		if (nome == null || nome.equals("")) {
 			throw new IllegalArgumentException();
@@ -32,7 +59,14 @@ public class Pessoa {
 		this.partido = "";
 		this.funcao = null;
 	}
-
+	/**
+	 * Inicia uma pessoa a partir do nome, dni, estado, interesses e partido.
+	 * @param nome
+	 * @param dni
+	 * @param estado
+	 * @param interesses
+	 * @param partido
+	 */
 	public Pessoa(String nome, String dni, String estado, String interesses, String partido) {
 		this(nome, dni, estado, interesses);
 
@@ -42,19 +76,31 @@ public class Pessoa {
 
 		this.partido = partido;
 	}
-
+	/**
+	 * Retorna o nome da pessoa
+	 * @return Uma String que representa o nome da pessoa
+	 */
 	public String getNome() {
 		return nome;
 	}
-
+	/**
+	 * Retorna o dni da pessoa
+	 * @return Uma String que representa o dni da pessoa
+	 */
 	public String getDni() {
 		return dni;
 	}
-
+	/**
+	 * Retorna o estado da pessoa
+	 * @return Uma String que representa o estado da pessoa
+	 */
 	public String getEstado() {
 		return estado;
 	}
-
+	/**
+	 * Retorna os interesses da pessoa;
+	 * @return Uma String que representa os interesses da pessoa
+	 */
 	public String getInteresses() {
 		if (this.interesses == null|| this.interesses.equals("")) {
 			return "";
@@ -63,7 +109,10 @@ public class Pessoa {
 			return " - Interesses: " + this.interesses;
 		}
 	}
-
+	/**
+	 * Retorna o partido da pessoa;
+	 * @return Uma String que representa o estado da pessoa
+	 */
 	public String getPartido() {
 		if (partido == null || partido.equals("")) {
 			return "";
@@ -72,15 +121,24 @@ public class Pessoa {
 			return " - " + this.partido;
 		}
 	}
-
+	/**
+	 * Altera a funcao da pessoa;
+	 * @param dataDeInicio da pessoa na nova funcao
+	 */
 	public void setFuncao(String dataDeInicio) {
 		this.funcao = new Deputado(dataDeInicio);
-	}
-
+	}                 
+	/**
+	 * Verifica se a pessoa exerce a funcao de deputado
+	 * @return Um booleano,se a pessoa exerce a funcao de deputado retorna true,
+	 * caso contrario retorna falso 
+	 */
 	public boolean isDeputado() {
 		return funcao != null;
 	}
-
+	/**
+	 * Retorna a representação textual da pessoa 
+	 */
 	@Override
 	public String toString() {
 		if (funcao == null) {
