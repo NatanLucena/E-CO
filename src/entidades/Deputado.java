09838d2cea53700740809b3bbf545b3ccefd8458
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2019-06-12 23:58:07 -0300

    Pastas desnecessarias removidas

--- src/entidades/Deputado.java
+++ src/entidades/Deputado.java
@@ -1,23 +1,50 @@
 package entidades;
 
+/**
+ * Responsavel por representar um Deputado
+ * @author NatanLucena, CayoViegas, lucas-lucena, JacksonMateus
+ *
+ */
 public class Deputado implements Exibir {
 
+	/**
+	 * Data de inicio que a pessoa exerceu a funcao de deputado
+	 */
 	private String dataDeInicio;
+	
+	/**
+	 * Quantidade de leis que o deputado aprovou
+	 */
 	private int leisAprovadas;
 
+	/**
+	 * Inicia o deputado a partir da data de inicio da pessoa na funcao de deputado
+	 * @param dataDeInicio que a pessoa exerceu a funcao de deputado
+	 */
 	public Deputado(String dataDeInicio) {
 		this.dataDeInicio = dataDeInicio;
 		this.leisAprovadas = 0;
 	}
 
+	/**
+	 * Retorna as quantidade de leis aprovadas pelo deputado
+	 */
 	public int getLeisAprovadas() {
 		return leisAprovadas;
 	}
+
+	/**
+	 * Altera a quantidade de leis aprovadas pelo deputado
+	 * @param leisAprovadas nova quantidade de leis aprovadas pelo deputado
+	 */
 
 	public void setLeisAprovadas(int leisAprovadas) {
 		this.leisAprovadas = leisAprovadas;
 	}
 
+	/**
+	 * @return Retorna a data de inicio da pessoa como Deputado
+	 */
 	public String getDataDeInicio() {
 		String dia = dataDeInicio.substring(0, 2);
 		String mes = dataDeInicio.substring(2, 4);
@@ -26,6 +53,15 @@ public class Deputado implements Exibir {
 		return dia + "/" + mes + "/" + ano;
 	}
 
+	/**
+	 * 
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * 
+	 */
 	@Override
 	public String exibir(String nome, String dni, String estado, String partido, String interesses) {
 		return "POL: " + nome + " - " + dni + " (" + estado + ")" + partido + interesses + " - "
