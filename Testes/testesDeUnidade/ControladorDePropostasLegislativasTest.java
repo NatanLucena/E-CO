58c200d5d7be08ad1e6617ad171432285bcd8b7f
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2019-06-18 13:41:31 -0300

    Junit Bizarre Adventures part 1: Test Tendency

--- Testes/testesDeUnidade/ControladorDePropostasLegislativasTest.java
+++ Testes/testesDeUnidade/ControladorDePropostasLegislativasTest.java
@@ -2,13 +2,27 @@ package testesDeUnidade;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControladorDePropostasLegislativasTest {
+import controladores.ControladorDePropostasLegislativas;
 
+class ControladorDePropostasLegislativasTest {
+	private ControladorDePropostasLegislativas controlador;
+
+	@BeforeEach
+	void iniciaControlador() {
+		this.controlador = new ControladorDePropostasLegislativas();
+	}
+	
 	@Test
 	void testCadastrarPL() {
-		fail("Not yet implemented");
+		assertEquals(this.controlador.cadastrarPL("Aaaaa", 2019, "Bbbbb", "Ccccc", "Ddddd", true), "PL 1/2019");
+		assertEquals(this.controlador.cadastrarPL("Eeeee", 2019, "Fffff", "Ggggg", "Hhhhh", false), "PL 2/2019");
+		assertEquals(this.controlador.cadastrarPL("Iiiii", 2017, "Jjjjj", "Kkkkk", "Lllll", true), "PL 1/2017");
+		assertEquals(this.controlador.cadastrarPL("Aaaaa", 2017, "Bbbbb", "Ccccc", "Ddddd", true), "PL 2/2017");
+		assertEquals(this.controlador.cadastrarPL("Eeeee", 2017, "Fffff", "Ggggg", "Hhhhh", false), "PL 3/2017");
+		assertEquals(this.controlador.cadastrarPL("Mmmmm", 2017, "Nnnnn", "Ooooo", "Ppppp", true), "PL 4/2017");
 	}
 
 	@Test
