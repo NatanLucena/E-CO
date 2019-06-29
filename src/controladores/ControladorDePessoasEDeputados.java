6e1060a7b03ab400a97e38384c4bd7114a44114f
NatanLucena <natan.lucena@ccc.ufcg.edu.br>
2019-06-28 21:36:31 -0300

    caso 9 quase implementado e design de votacao em comissao melhorado

--- src/controladores/ControladorDePessoasEDeputados.java
+++ src/controladores/ControladorDePessoasEDeputados.java
@@ -8,6 +8,11 @@ import java.util.Map;
 
 import entidades.Deputado;
 import entidades.Pessoa;
+import entidades.PropostaLegislativa;
+import estrategias.Estrategia;
+import estrategias.EstrategiaAprovacao;
+import estrategias.EstrategiaConclusao;
+import estrategias.EstrategiaConstitucional;
 import metodosAuxiliares.ValidadorGeral;
 
 /**
@@ -226,7 +231,7 @@ public class ControladorDePessoasEDeputados implements Serializable {
 	public List<String> getListaDeInteresses(String dni) {
 		validadorGeral.validaNullOuVazio(dni, "Erro ao recuperar lista de interesses de um deputado: dni nao pode ser vazio ou nulo.");
 		validadorGeral.validaDni(dni, "Erro ao recuperar lista de interesses de um deputado: dni inavlido.");
-		return deputados.get(dni).getListaDeInteresses();
+		return pessoas.get(dni).getListaDeInteresses();
 	}
 
 	/**
@@ -239,30 +244,34 @@ public class ControladorDePessoasEDeputados implements Serializable {
 	}
 
 	/**
+	 * Metodo que altera o tipo de estrategia de desempate para a pessoa passada como parametro.
 	 * 
-	 * @param dni
+	 * @param dni dni da pessoa que tera sua estategia alterada
 	 * 
-	 * @param estrategia
+	 * @param estrategia o novo tipo de estrategia que sera implementado
 	 * 
 	 */
-	public void cadastrarEstrategia(String dni, String estrategia) {
-		validadorGeral.validaNullOuVazio(dni, "");
-		validadorGeral.validaDni(dni, "");
-		validadorGeral.validaNullOuVazio(estrategia, "");
-		deputados.get(dni).setEstrategia(estrategia);
+	public void setEstrategia(String dni, String estrategia) {
+		Estrategia novaEstrategia;
+		if(estrategia.equalsIgnoreCase("CONCLUSAO")) {
+			novaEstrategia = new EstrategiaConclusao();
+		}else if(estrategia.equalsIgnoreCase("APROVACAO")) {
+			novaEstrategia = new EstrategiaAprovacao();
+		}else {
+			novaEstrategia = new EstrategiaConstitucional();
+		}
+		pessoas.get(dni).setEstrategia(novaEstrategia);
 	}
 	
 	/**
+	 * Retorna a proposta mais adequada, para a pessoa passada, dentre as propostas da lista.
 	 * 
-	 * @param dni
+	 * @param dni dni da pessoa que ira executar o desempate
 	 * 
-	 * @return
+	 * @return a proposta mais adequada a pessoa que foi passada como parametro
 	 * 
 	 */
-	public String recuperaEstrategia(String dni) {
-		validadorGeral.validaNullOuVazio(dni, "");
-		validadorGeral.validaDni(dni, "");
-		
-		return deputados.get(dni).getEstrategia();
+	public String desempate(String dni, List<PropostaLegislativa> propostas) {
+		return pessoas.get(dni).desempate(propostas);
 	}
 }
--- src/estrategias/EstrategiaConstitucional.java
+++ src/estrategias/EstrategiaConstitucional.java
@@ -1,15 +1,18 @@
 package estrategias;
 
+import java.util.Collections;
 import java.util.List;
 
 import entidades.PropostaLegislativa;
+import metodosAuxiliares.ComparadorDePropostasConstitucionais;
 
 public class EstrategiaConstitucional implements Estrategia {
 
 	@Override
 	public String desempate(List<PropostaLegislativa> propostas) {
-		// TODO Auto-generated method stub
-		return null;
+		ComparadorDePropostasConstitucionais comparadorConstitucional = new ComparadorDePropostasConstitucionais();
+		Collections.sort(propostas, comparadorConstitucional);
+		return propostas.get(0).getCodigo();
 	}
 	
 }
