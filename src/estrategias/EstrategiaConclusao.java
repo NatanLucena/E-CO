4c893cbbb3c42f75816fcbedf3a5badf5da2a5f5
lucas-lucena <lucas.lucena@ccc.ufcg.edu.br>
2019-06-30 19:47:06 -0300

    Revert "Revert "Merge branch 'case6/7' of https://github.com/NatanLucena/E-CO into case6/7""
    This reverts commit 96897c4715d4ecd17b4d1101105e9407af3fd6dd.

--- src/estrategias/EstrategiaConclusao.java
+++ src/estrategias/EstrategiaConclusao.java
@@ -1,15 +1,18 @@
 package estrategias;
 
+import java.util.Collections;
 import java.util.List;
 
 import entidades.PropostaLegislativa;
+import metodosAuxiliares.ComparadorDePropostasConclusao;
 
 public class EstrategiaConclusao implements Estrategia {
 
 	@Override
 	public String desempate(List<PropostaLegislativa> propostas) {
-		// TODO Auto-generated method stub
-		return null;
+		ComparadorDePropostasConclusao comparaConclusao = new ComparadorDePropostasConclusao();
+		Collections.sort(propostas, comparaConclusao);
+		return propostas.get(0).getCodigo();
 	}
 
 }
